74461addafbc73b51d356e36d3790c03eb12f025
Hasan Basri Şamlı <hasan.basrisamli@hotmail.com>
2025-06-22 19:03:29 +0300

    terraform files re-organized

--- main.tf
+++ main.tf
@@ -1,57 +1,20 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "6.0.0"
+      source  = "hashicorp/aws" 
+      version = "6.0.0" # The version of the AWS provider
     }
   }
 
   backend "s3" {
-    bucket         = "mystates-terraform-state"
-    key            = "state/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "mystates-terraform-state-lock"
-    encrypt        = true
+    bucket         = "mystates-terraform-state" # The name of the S3 bucket
+    key            = "state/terraform.tfstate" # The key for the S3 bucket
+    region         = "us-east-1" # The region for the S3 bucket
+    dynamodb_table = "mystates-terraform-state-lock" # DynamoDB table for state locking
+    encrypt        = true # Encrypt the state file
   }
 }
 
 provider "aws" {
   region = var.aws_region
-}
-
-resource "aws_iam_openid_connect_provider" "github" {
-  url             = "https://token.actions.githubusercontent.com"
-  client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = ["6938fd4d98bab03faadb97b343e59a7684ecb4ae"]
-}
-
-resource "aws_iam_role" "github_actions" {
-  name = "GithubActionsRole"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Principal = {
-          Federated = aws_iam_openid_connect_provider.github.arn
-        }
-        Action = "sts:AssumeRoleWithWebIdentity"
-        Condition = {
-          StringLike = {
-            "token.actions.githubusercontent.com:sub": "repo:basrihsn/rsschool-devops-course-tasks:*"
-          },
-          StringEquals = {
-            "token.actions.githubusercontent.com:aud": "sts.amazonaws.com"
-          }
-        }
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "github_actions_policies" {
-  for_each   = toset(var.iam_policy_names)
-
-  role       = aws_iam_role.github_actions.name 
-  policy_arn = "arn:aws:iam::aws:policy/${each.value}"
 }
