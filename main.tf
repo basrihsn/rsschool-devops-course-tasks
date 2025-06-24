7d418c392fb3be0aa8d9198d7a6f699cf5c70d02
Hasan Basri Şamlı <hasan.basrisamli@hotmail.com>
2025-06-24 13:55:11 +0300

    main.tf updated

--- main.tf
+++ main.tf
@@ -1,21 +1,20 @@
- terraform {
-  required_version = ">= 1.0"
+terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
+      source  = "hashicorp/aws" 
+      version = "6.0.0" # The version of the AWS provider
     }
+  }
+
+  backend "s3" {
+    bucket         = "mystates-terraform-state" # The name of the S3 bucket
+    key            = "state/terraform.tfstate" # The key for the S3 bucket
+    region         = "us-east-1" # The region for the S3 bucket
+    dynamodb_table = "mystates-terraform-state-lock" # DynamoDB table for state locking
+    encrypt        = true # Encrypt the state file
   }
 }
 
 provider "aws" {
   region = var.aws_region
-
-  default_tags {
-    tags = {
-      Project     = var.project_name
-      Environment = var.environment
-      ManagedBy   = "Terraform"
-    }
-  }
-} 
+}
